30ae0e3000947940941c85f0c42e4299030ad1a3
Abubakar Janka <jpacybc@gmail.com>
2025-11-20 20:50:46 -0300

    Add Render deployment configuration

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,6 +2,7 @@
 QuickBite Connect - Production Settings
 """
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
@@ -20,6 +21,22 @@ DATABASES = {
     }
 }
 
+
+# Database - use dj_database_url to parse DATABASE_URL
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
+
+# Add this for Render
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost').split(',')
+
+# Static files on Render
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
